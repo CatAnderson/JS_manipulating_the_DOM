d958f019c68f7d080f21b5f3b4f440d0bcc9d514
CatAnderson <thecatanderson100@gmail.com>
2020-10-16 13:40:48 +0100

    added new list with h1, h3 & p...appended these inputs to the list

--- js/app.js
+++ js/app.js
@@ -9,10 +9,19 @@ const handleSave = function(event){
     event.preventDefault();
 
     //make new list item with h2, h3 & p in this.
+    const newMovieListItem = document.createElement('li');
+    const titleInput = document.createElement('h2');
+    const releaseDateInput = document.createElement('h3');
+    const genreInput = document.createElement('p');
+
     //append this to the ul already in the html
+    newMovieListItem.appendChild(titleInput);
+    newMovieListItem.appendChild(releaseDateInput);
+    newMovieListItem.appendChild(genreInput);
+
 
     //create movieList
     //grab values from the form and put them in the list.
     //append the values to the movieList.
-    
+
 }
