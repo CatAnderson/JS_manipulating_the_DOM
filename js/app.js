8b9e950707dc9dad21dc21a1fe2fd5c57c0cafad
CatAnderson <thecatanderson100@gmail.com>
2020-10-16 13:17:06 +0100

    set up HTML, working on app.js

---
+++ js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript loaded');
+
+    const save = document.querySelector('#new-movie-form')
+    save.addEventListener('submit',handleSave)
+});
+
+const handleSave = function(event){
+    event.preventDefault();
+
+    //make new list item with h2, h3 & p in this.
+    //append this to the ul already in the html
+
+    //create movieList
+    //grab values from the form and put them in the list.
+    //append the values to the movieList.
+    
+}
